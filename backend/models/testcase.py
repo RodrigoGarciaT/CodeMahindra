4d5296077178a0ff5348437ca09819365a431134
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2025-05-01 12:26:54 -0600

    Submission kinda works

--- backend/models/testcase.py
+++ backend/models/testcase.py
@@ -8,3 +8,6 @@ class TestCase(Base):
     input = Column(Text)
     output = Column(Text)
     problem_id = Column(Integer, ForeignKey("Problem.id", ondelete="CASCADE"))
+    
+    def __str__(self):
+        return f"TestCase(id={self.id}, input='{self.input}', output='{self.output}')"
