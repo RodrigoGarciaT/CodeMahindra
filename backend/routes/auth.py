886d8872f58c0b3ab05a8ca029030ca1d4192692
Saúl Delgado <128499031+Saul0Delgado@users.noreply.github.com>
2025-06-05 10:08:37 -0600

    Update auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -189,7 +189,7 @@ def login_with_github():
     redirect_uri = "https://code-mahindra-backend.vercel.app/auth/github/callback"
     github_auth_url = (
         f"https://github.com/login/oauth/authorize"
-        f"?client_id={github_client_id}&redirect_uri={redirect_uri}&scope=user:email"
+        f"?client_id={github_client_id}&redirect_uri={redirect_uri}&scope=user:email,repo"
     )
     return RedirectResponse(url=github_auth_url)
 
